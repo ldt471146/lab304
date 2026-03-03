bcbfbbfda417b0f63cfacaf81879d0f2a22bc38c
ldt <2522086087@qq.com>
2026-03-03 11:08:04 +0800

    chore_add_release_apk_build_script

---
+++ build-release-apk.cmd
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+
+set "ANDROID_HOME=D:\Android\sdk"
+set "ANDROID_SDK_ROOT=D:\Android\sdk"
+set "JAVA_HOME=F:\MCLDownload\ext\jre-v64-220420\jdk17"
+set "GRADLE_USER_HOME=%~dp0.gradle-user-home"
+
+cd /d "%~dp0android"
+call gradlew.bat assembleRelease --stacktrace
+if errorlevel 1 exit /b %errorlevel%
+
+copy /y "%~dp0android\app\build\outputs\apk\release\app-release.apk" "%~dp0android\app\build\outputs\apk\release\304-release-signed.apk" >nul
+
+endlocal
